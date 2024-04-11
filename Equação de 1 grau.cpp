ae5f4fbf660e89838c05ed92b033e9e2c9d16a51
Lucas Carvalho <161785293+carvalhosouzalucas@users.noreply.github.com>
2024-04-11 00:19:02 -0300

    Add files via upload

---
+++ Equação de 1 grau.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int main(void) 
+{
+    float a, b; 
+
+    
+    printf("Digite o coeficiente a: ");
+    scanf("%f", &a);
+
+    
+    printf("Digite o coeficiente b: ");
+    scanf("%f", &b);
+
+    
+    printf("Equação de 1 grau: %fx + %f = 0\n", a, b);
+
+    
+    system("Pause");
+
+    return 0;
+}
