59682257817c7d9fbb1ad353dbd2d3a9fe381221
Lucas Carvalho <161785293+carvalhosouzalucas@users.noreply.github.com>
2024-04-21 13:26:18 -0300

    Add files via upload

---
+++ Numero divisivel por 5.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+	int num;
+	printf("Informe o numero:\n");
+	scanf("%d", &num);
+	if (num % 5 == 0)
+	   printf("O número %d é divisível por 5\n", num);
+	else
+	   printf("O número %d não é divisível por 5\n", num);
+	   
+	   
+	return (0);
+}
